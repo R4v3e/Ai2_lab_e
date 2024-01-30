be1778c2cb1e39d009c84fd796b6a838e4c51f55
R4v3e <feanor181@outlook.com>
2024-01-30 18:35:59 +0100

    lab-a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component'; // Import HomeComponent
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // imports: [HomeComponent] // Include HomeComponent in the imports array
 })
 export class AppComponent {
-  title = 'lab-a';
+  title = 'homes';
 }
